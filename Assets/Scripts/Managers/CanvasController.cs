93aeaa6be861fdd416622fde7948a69a790bdf31
Damian <spell2cast@gmail.com>
2017-04-11 17:36:45 +0200

    Movement points and dice further animations

--- Assets/Scripts/Managers/CanvasController.cs
+++ Assets/Scripts/Managers/CanvasController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,8 +8,11 @@ using UnityEngine.UI;
 public class CanvasController : MonoBehaviour {
 
 	private List<Transform> Controls;
+	private string CurrentDiceVal;
+	// private bool diceTilted;
 
 	private void Start() {
+		CurrentDiceVal = "00";
 		Controls = new List<Transform>();
 		foreach (Transform child in transform) {
 			Controls.Add(child);
@@ -16,16 +20,67 @@ public class CanvasController : MonoBehaviour {
 		PlayerManager.TurnStarted += ShowNewTurnText;
 		PlayerManager.TurnStarted += ShowDice;
 		PlayerManager.GameBusy += NewTurnButtonEnabled;
+		//PlayerManager.PathWalkStarted += HideResult;
+		PlayerManager.PathTicked += UpdateResult;
 		AEDice.DiceAnimEnd += NewTurnButtonEnabled;
+		AEDice.DiceAnimResult += ShowResult;
 	}
 
 	private void ShowDice () {
 		Transform image = Controls.Where(obj => obj.name == "DiceRoll").SingleOrDefault();
 		Transform button = Controls.Where(obj => obj.name == "DiceRollButton").SingleOrDefault();
-
+		// diceTilted = false;
 		if (button.GetComponent<Button>().interactable == false) {
 			button.GetComponent<Button>().interactable = true;
 			image.GetComponent<Animator>().SetTrigger("ResetDice");
+		}
+	}
+
+	private void ShowResult() {
+		Transform text = Controls.Where(obj => obj.name == "DiceResult").SingleOrDefault();
+		CurrentDiceVal = PlayerManager.Master.RollMovementPoints();
+		text.GetComponent<Text>().text = CurrentDiceVal;
+		text.GetComponent<Text>().enabled = true;
+		text.GetComponent<Animator>().SetTrigger("ShowResult");
+	}
+
+	private void UpdateResult() {
+		Transform text = Controls.Where(obj => obj.name == "DiceResult").SingleOrDefault();
+		string str = CurrentDiceVal;
+
+		if (str.Length == 2) {
+			int b = Int32.Parse(str[1].ToString());
+			if (b > 1) {
+				b--;
+				str = (str[0].ToString()) + (b.ToString());
+			}
+			else {
+				str = str[0].ToString();
+				//if (diceTilted == false) {
+					// diceTilted = true;
+					text.GetComponent<Animator>().SetTrigger("Tilt");
+				//}
+			}
+		}
+		else if (str.Length == 1) {
+
+			int a = Int32.Parse(str[0].ToString());
+			if (a > 1) {
+				a--;
+				str = a.ToString();
+			}
+			else str = "";
+		}
+		CurrentDiceVal = str;
+		text.GetComponent<Text>().text = str;
+	}
+
+	public void HideResult() {
+		Transform text = Controls.Where(obj => obj.name == "DiceResult").SingleOrDefault();
+		AnimatorStateInfo Animation = text.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
+		if (Animation.IsName("Result") || Animation.IsName("DiceTilt")) {
+			text.GetComponent<Text>().enabled = false;
+			text.GetComponent<Animator>().SetTrigger("EndTurn");
 		}
 	}
 
@@ -37,6 +92,7 @@ public class CanvasController : MonoBehaviour {
 	private void NewTurnButtonEnabled() {
 		Transform button = Controls.Where(obj => obj.name == "TurnButton").SingleOrDefault();
 		button.GetComponent<Button>().interactable = true;
+
 	}
 
 	private void NewTurnButtonEnabled(bool enabled) {
@@ -46,7 +102,11 @@ public class CanvasController : MonoBehaviour {
 
 	private void OnDestroy() {
 		PlayerManager.TurnStarted -= ShowNewTurnText;
+		PlayerManager.TurnStarted -= ShowDice;
 		PlayerManager.GameBusy -= NewTurnButtonEnabled;
+		//PlayerManager.PathWalkStarted -= HideResult;
+		PlayerManager.PathTicked -= UpdateResult;
+		AEDice.DiceAnimEnd -= NewTurnButtonEnabled;
+		AEDice.DiceAnimResult -= ShowResult;
 	}
-
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -9,6 +9,10 @@ public class PlayerManager : GenericSingletonClass<PlayerManager>
 	public static event TurnStartDelegate TurnStarted;
 	public delegate void GameBusyDelegate(bool enabled);
 	public static event GameBusyDelegate GameBusy;
+	// public delegate void PathWalkStartedDelegate();
+	// public static event PathWalkStartedDelegate PathWalkStarted;
+	public delegate void PathTickedDelegate();
+	public static event PathTickedDelegate PathTicked;
 
 	public List<Player> Players;
 	private static Player CurrentPlayer;
@@ -19,7 +23,7 @@ public class PlayerManager : GenericSingletonClass<PlayerManager>
 	private int PlayerCount;
 
 	void Start () {
-		AEDice.DiceAnimResult += RollMovementPoints;
+		// AEDice.DiceAnimResult += RollMovementPoints;
 		PlayerCount = Players.Count;
 		PlayerIndex = 0;
 		TurnStart();
@@ -36,7 +40,6 @@ public class PlayerManager : GenericSingletonClass<PlayerManager>
 		CycleNextPlayer();
 		FieldManager.Master.ClearAllFields();
 		TunrDiceRoll();
-		// TurnPathMark();
 	}
 
 	private void CycleNextPlayer() {
@@ -60,16 +63,16 @@ public class PlayerManager : GenericSingletonClass<PlayerManager>
 	private void TurnPathMarked(Field targetField) {	// Wait for confirmation stage (path can be redefinied)
 		FieldManager.Master.MarkPath(targetField, MovementPoints);
 		if (CurrentPath != null && IsSubscribedStartWalk == false) {    // Before allowing to go down this path, check if it's correct
-			Debug.Log("Entered" + CurrentPath);
 			IsSubscribedStartWalk = true;
 			InputManager.KeySpace += TurnPathStartWalk;
 		}
 	}
 	
 	private void TurnPathStartWalk () {     // Path travel stage, animate movement (can't mark new path/start new walk)
-		if (GameBusy != null) {
+		// if (PathWalkStarted != null)
+		//	PathWalkStarted();
+		if (GameBusy != null)
 			GameBusy(false);
-		}
 		InputManager.MousePressed -= TurnPathMarked;
 		InputManager.KeySpace -= TurnPathStartWalk;
 		List<Field> walkablePath = new List<Field>();
@@ -79,20 +82,18 @@ public class PlayerManager : GenericSingletonClass<PlayerManager>
 
 	private IEnumerator PathTick(List<Field> path) {
 
-		//List<Field> path = currentPath;  
 		float movementSpeed = path.Count < 4 ? 0.325f : 1.30f / path.Count;
 
 		for (int i = 0; i < path.Count; i++) {
 			MovementPoints--;
+			if (PathTicked != null)
+				PathTicked();
 			CurrentPlayer.SetPosition(path[i]);
 			yield return new WaitForSeconds(movementSpeed);
 			FieldManager.Master.ClearField(path[i]);
 		}
 
 		CurrentPath = null;     // Remove current path from memory, so it has to be marked again in new turn
-		//if (MovementPoints == 0)
-		//	TurnStart();
-		//else
 		if (GameBusy != null) {
 			GameBusy(true);
 		}
@@ -121,13 +122,16 @@ public class PlayerManager : GenericSingletonClass<PlayerManager>
 		CurrentPath = path;
 	}
 
-	public void RollMovementPoints() {
-		MovementPoints = Random.Range(1, 6) + Random.Range(1, 6);
+	public string RollMovementPoints() {
+		int a = Random.Range(1, 7);
+		int b = Random.Range(1, 7);
+		MovementPoints = a + b;
+		return a.ToString() + b.ToString();
 	}
 
 	// Destroy
 
 	private void OnDestroy() {
-		AEDice.DiceAnimResult -= RollMovementPoints;
+		// AEDice.DiceAnimResult -= RollMovementPoints;
 	}
 }
